2442b717278e4f28f809bd18ed3b365b5bc9abc3
ffd8 <ted@ffd8.org>
2019-12-09 12:09:54 +0100

    Initial commit

---
+++ tools/tool1/sketch.js
@@ -0,0 +1,29 @@
+let layer2d;
+
+function setup() {
+	createCanvas(windowWidth, windowHeight, WEBGL); // set 3d space
+	layer2d = createGraphics(width * .4, height * .4); // create 2d layer
+	//layer2d.background(255, 255, 0, 50)
+	angleMode(DEGREES)
+}
+
+function draw() {
+	background(0, 0, 255);
+	
+	orbitControl(5)
+	// layer2d.clear();
+	layer2d.ellipse(random(width), random(height), 10);
+	layer2d.stroke(0, 255, 255)
+	layer2d.strokeWeight(5);
+	if(mouseIsPressed) {
+		layer2d.line(mouseX, mouseY, pmouseX, pmouseY);
+	}
+	// image(layer2d, 0, 0) // don't draw as boring image
+
+	texture(layer2d); // use as texture for 3d
+	noStroke(); // remove lines of 3d
+
+	rotateY(frameCount / 2);
+	//plane(layer2d.width, layer2d.height); // 2d like image but rotates
+	torus(300, 50);
+}
